4d6baedd8a5e817db6cc31f6dbf75ed3d05e960c
jean pirul <jeanpirul@gmail.com>
2022-01-28 21:41:21 -0300

    second changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Navbar/Sidebar/Sidebar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar/Sidebar/Sidebar.tsx
@@ -0,0 +1,9 @@
+function Sidebar() {
+  return <div>
+      <h1>
+          Hola soy cri y me gusta el pene a demas soy el sidebar
+      </h1>
+  </div>;
+}
+
+export default Sidebar;
